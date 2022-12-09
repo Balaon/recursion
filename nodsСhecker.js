3e72d8cacbab189fc422e68fb504ef88b563c737
Balaon <49208121+Balaon@users.noreply.github.com>
2022-12-09 04:21:44 +0300

    Update nodsСhecker.js
    refactor

--- nodsСhecker.js
+++ nodsСhecker.js
@@ -1,14 +1,16 @@
-let body = document.body;
-function nodsСhecker(value, path = '') {
-    let cont = 0;
-    let structure = [...value.children];
-    for (let elem of structure) {
-        if (elem.querySelector('*') === null) {
-            console.log(`${path} -> ${elem.tagName}`);
-        } else {
-            cont++;
-            nodsСhecker(elem, `-> ${'---- '.repeat(cont)}->${elem.tagName}`);
-        }
-    }
-}
-nodsСhecker(body);
+ //выводит структуру вложенности тегов body в консоль
+
+(function nodsСhecker(value, step = 0) {
+  let count = step;
+    
+  for (let elem of [...value.children]) {
+    console.group(elem.tagName);
+        
+    if (elem.querySelector('*') === null) { console.groupEnd(); continue;}
+    
+     nodsСhecker(elem,  count++); 
+  }
+    
+  console.groupEnd();
+})(document.body)
+
